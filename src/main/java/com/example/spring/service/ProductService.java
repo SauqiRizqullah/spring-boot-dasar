c24829cd0d1257a5407eb41d9abe556dc3e0679a
Tommy <tommyharyanto72@gmail.com>
2024-11-22 22:07:08 +0700

    [ADD] Autowired (Menentukan default constructor)

--- src/main/java/com/example/spring/service/ProductService.java
+++ src/main/java/com/example/spring/service/ProductService.java
@@ -2,6 +2,7 @@ package com.example.spring.service;
 
 import com.example.spring.repository.ProductRepository;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +11,13 @@ public class ProductService {
     @Getter
     private ProductRepository productRepository;
 
+//    No Default Constructor Found
+    @Autowired // Digunakan untuk menentukan constructor default
     public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    public ProductService(ProductRepository productRepository, String name){
         this.productRepository = productRepository;
     }
 }
