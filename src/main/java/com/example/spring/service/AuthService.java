1a3188684b3b77ad04af6ad04dd00fd80981c3fb
Muhammad Sauqi Rizqullah <sauqi.risqullah@gmail.com>
2024-12-06 16:18:28 +0700

    [ADD] Aware

---
+++ src/main/java/com/example/spring/service/AuthService.java
@@ -0,0 +1,30 @@
+package com.example.spring.service;
+
+import lombok.Getter;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthService implements ApplicationContextAware, BeanNameAware {
+
+    @Getter
+    private ApplicationContext applicationContext;
+
+    @Getter
+    private String beanName;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        this.beanName = name;
+    }
+}
