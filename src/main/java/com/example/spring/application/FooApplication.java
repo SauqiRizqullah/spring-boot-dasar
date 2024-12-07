90654b042862bd487c86f37f26ec2d47e9cc9848
Muhammad Sauqi Rizqullah <sauqi.risqullah@gmail.com>
2024-12-07 17:25:21 +0700

    [ADD] Customize Spring Application

--- src/main/java/com/example/spring/application/FooApplication.java
+++ src/main/java/com/example/spring/application/FooApplication.java
@@ -2,6 +2,7 @@ package com.example.spring.application;
 
 import com.example.spring.data.Bar;
 import com.example.spring.data.Foo;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,12 +12,21 @@ import org.springframework.context.annotation.Bean;
 public class FooApplication {
 
     @Bean
-    public Foo foo(Bar bar){
+    public Foo foo(){
         return new Foo();
     }
+//
+//    public static void main(String[] args) {
+//        ConfigurableApplicationContext context = SpringApplication.run(FooApplication.class, args);
+//        Foo foo = context.getBean(Foo.class);
+//        System.out.println(foo);
+//    }
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(FooApplication.class, args);
+        SpringApplication application = new SpringApplication(FooApplication.class);
+        application.setBannerMode(Banner.Mode.OFF); // salah satu method untuk mengubah setting aplikasi spring boot
+
+        ConfigurableApplicationContext context = application.run(args);
         Foo foo = context.getBean(Foo.class);
         System.out.println(foo);
     }
