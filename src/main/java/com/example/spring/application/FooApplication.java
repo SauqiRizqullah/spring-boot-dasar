6c15ae2cf91656e7658aa007c67b6277ea7e83bb
Muhammad Sauqi Rizqullah <sauqi.risqullah@gmail.com>
2024-12-08 09:52:42 +0700

    [ADD] Spring Application Event

--- src/main/java/com/example/spring/application/FooApplication.java
+++ src/main/java/com/example/spring/application/FooApplication.java
@@ -2,11 +2,14 @@ package com.example.spring.application;
 
 import com.example.spring.data.Bar;
 import com.example.spring.data.Foo;
+import com.example.spring.listener.AppStartingListener;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class FooApplication {
@@ -25,6 +28,7 @@ public class FooApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(FooApplication.class);
         application.setBannerMode(Banner.Mode.OFF); // salah satu method untuk mengubah setting aplikasi spring boot
+        application.setListeners(List.of(new AppStartingListener()));
 
         ConfigurableApplicationContext context = application.run(args);
         Foo foo = context.getBean(Foo.class);
